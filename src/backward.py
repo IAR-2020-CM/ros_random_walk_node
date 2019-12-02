54d9654d8ce9c32d274a67b7ac1d9d880d1095ce
turtlebot_LYON <simon.bultmann@insa-lyon.fr>
2019-12-02 16:50:09 +0100

    fix: remove useless lines

--- src/backward.py
+++ src/backward.py
@@ -1,15 +1,9 @@
 #!/usr/bin/env python
 import rospy
 from std_msgs.msg import String
-import rospy
 from geometry_msgs.msg import Twist
-import numpy
-from sensor_msgs.msg import LaserScan, PointCloud2
-import sys, tty, termios
-from cv_bridge import CvBridge, CvBridgeError
+from sensor_msgs.msg import LaserScan
 import numpy as np
-import cv2
-import cv2.cv as cv
 import math
 import random
 
@@ -37,15 +31,6 @@ def camera_callback(msg):
     if len(scan) != 0:
         dist = scan
 
-    #try:
-    #    depth_image = bridge.imgmsg_to_cv(msg, "32FC1")
-    #    dl = depth_image[depth_image.height // 2, :]
-    #    depth_line = [dl[0, i] for i in range(dl.width) if not numpy.isnan(dl[0, i])]
-    #    rospy.loginfo("Line "  + str(min(depth_line)) )
-    #except CvBridgeError, e:
-    #    print e
-
-
 def autonomous_move():
     global dist
 
@@ -61,7 +46,7 @@ def autonomous_move():
         s = "hello wold %s" % rospy.get_time()
         #rospy.loginfo(s)
 
-        if any(not numpy.isnan(d) and d < 1 for d in dist):
+        if any(not np.isnan(d) and d < 1 for d in dist):
             linear = 0
             if not obstacle:
                 angular = random_sign()
